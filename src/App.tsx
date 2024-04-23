7aff665187a55b114a1de3f5294e92d0012d8b0e
GabrielMatiasn10 <gm4037003@gmail.com>
2024-04-23 10:02:08 -0300

    inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
+import  Header  from './components/Header';
+import  Footer  from './components/Footer';
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import  styles  from './components/App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Header />
+    <main className={styles.main}>
+    <h1>conteudo</h1>
+    </main>
+    <Footer />
+
+   </div>
+   
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+        <header className="App-header">
+         <h1>todo list</h1>
+        </header>
+       
+        </div>  
+    )
+}
+
+export default Header;
