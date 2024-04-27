3ccc877199e57d26148b587aefe6b470a9a85fe0
GabrielMatiasn10 <gm4037003@gmail.com>
2024-04-26 22:07:39 -0300

    day 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
 import  Header  from './components/Header';
 import  Footer  from './components/Footer';
-import React from 'react';
-
-import './App.css';
 import  styles  from './App.module.css';
-import TaskForm from './components/TaskForm';
+import { TaskForm } from './components/TaskForm';
 import TaskList from './components/TaskList';
+import {ITask} from './interfaces/Task';
+import { useState } from 'react';
 
 function App() {
+
+  const [TaskList, setTaskList] = useState<ITask[]>([]);
+
   return (
-   <div>
-    <Header />
-    <div>
-      <main className={styles.main}>
-        <h2>O Que voce vai fazer?</h2>
-       <TaskForm btnText='Criar tesk'/>
-       <div>
-      <h2>Suas tarefa:</h2>
-      <TaskList />
+   <><div>
+      <Header />
+      <div>
+        <main className={styles.main}>
+          <h2>O Que voce vai fazer?</h2>
+          <TaskForm
+            btnText='Adicionar Tarefa'
+            TaskList={TaskList}
+            setTaskList={setTaskList} />
+          <div>
+            <h2>Suas tarefa:</h2>
+
+          </div>
+        </main>
       </div>
-      </main>
     </div>
-  
     <Footer />
-   </div>
+    </>
+
   );
 }
 
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,26 +1,58 @@
-import React from 'react'
+import React, {ChangeEvent, FormEvent, useEffect} from 'react'
 
 import  styles  from './TaskForm.module.css';
+import {ITask} from '../interfaces/Task';
+import { useState } from 'react';
+import TaskList from './TaskList';
 
 interface Props {
     btnText: string
+    TaskList: ITask[]
+    setTaskList: React.Dispatch<React.SetStateAction<ITask[]>>
     }
     
 
-const TaskForm = ({ btnText }: { btnText: string }) => {
-    return (
-     <form className={styles.form}>
-        <div className={styles.input_container}>
-                <label htmlFor="title">Titulo</label>
-                <input type="text" id="title" name="title" placeholder='titulo da tarefa'/>
-        </div>
-        <div className={styles.input_container}>
-                <label htmlFor="difficulty">Dificuldade</label>
-                <input id="difficulty" name="difficulty" placeholder='Dificuldade'></input>
-     </div>
-     <input type="submit" value={btnText}/>
-     </form>
-    )
-}
-
-export default TaskForm
+    export const TaskForm = ({ btnText, TaskList, setTaskList }: Props) => {
+        const [id, setId] = useState<number>(0);
+        const [title, setTitle] = useState<string>('');
+        const [difficulty, setDifficulty] = useState<number>(0);
+    
+        const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+    
+            const id = Math.floor(Math.random() * 1000);
+    
+            const newTask: ITask = {
+                id: id,
+                title: title,
+                dificulty: difficulty,
+            };
+    
+            setTaskList([...TaskList, newTask]);
+            setTitle('');
+            setDifficulty(0);
+        };
+    
+        const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+            const { name, value } = e.target;
+            if (name === 'title') {
+                setTitle(value);
+            } else {
+                setDifficulty(Number(value));
+            }
+        };
+    
+        return (
+            <form onSubmit={addTaskHandler} className={styles.form}>
+                <div className={styles.input_container}>
+                    <label htmlFor="title">Titulo</label>
+                    <input type="text" id="title" name="title" placeholder="titulo da tarefa" onChange={handleChange} />
+                </div>
+                <div className={styles.input_container}>
+                    <label htmlFor="difficulty">Dificuldade</label>
+                    <input id="difficulty" name="difficulty" placeholder="Dificuldade" onChange={handleChange} />
+                </div>
+                <input type="submit" value={btnText} />
+            </form>
+        );
+    };
